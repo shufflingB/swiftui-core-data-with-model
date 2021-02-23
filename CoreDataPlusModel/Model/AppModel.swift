fc9ecd32c809f6888c0c83af6d68ef04a7903a65
Jonathan Hume <jonathan.hume@gmail.com>
2021-02-23 16:14:24 +0000

    Removed dead file

--- CoreDataPlusModel/Model/AppModel.swift
+++ CoreDataPlusModel/Model/AppModel.swift
@@ -67,34 +67,30 @@ class AppModel: ObservableObject {
 
     private var cancellableSet: Set<AnyCancellable> = []
 
-    
     let persistentContainer: NSPersistentContainer
-    
+
     init(inMemory: Bool = false) {
         persistentContainer = NSPersistentContainer(name: "AppCoreData")
         if inMemory {
             persistentContainer.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        persistentContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        persistentContainer.loadPersistentStores(completionHandler: { _, error in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 
                 /*
-                Typical reasons for an error here include:
-                * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                * The device is out of space.
-                * The store could not be migrated to the current model version.
-                Check the error message to determine what the actual problem was.
-                */
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
-        
-        
-        
-        
+
         print("Setting up Core Data update subscriptions  ")
         CoreDataPublisher(request: Item.fetchItemsAll(), context: persistentContainer.viewContext)
             .sink(
